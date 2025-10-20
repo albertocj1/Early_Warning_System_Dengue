3661c09fe0fdfe2a48f090e33db3f3ab71fa0fd8
albertocj1 <albertochristianjoshua@gmail.com>
2025-10-21 01:32:57 +0800

    Update streamlit_app.py

--- App/streamlit_app.py
+++ App/streamlit_app.py
@@ -24,7 +24,7 @@ WEATHER_API_KEY = "9c8585dd43864b27a66224931251910"
 # 📦 MODEL PATHS
 # -------------------------------------
 MODEL_PATH = "Model/dengue_classification_model.keras"
-SCALER_PATH = "Model/scaler_classificatio.pkl"
+SCALER_PATH = "Model/scaler_classification.pkl"
 
 # -------------------------------------
 # 🌍 STATIC CITY DATA
